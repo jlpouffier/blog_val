fdb7a381e955329930e655aa36ac9484cd105b1b
jlpouffier <jean.loic.pouffier@gmail.com>
2020-05-07 19:57:05 +0200

    redcarpet + links between category and recipe

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def markdown(text)
+		options = [:hard_wrap, :filter_html, :no_intra_emphasis, :autolink, :underline, :highlight, :quote, :tables, :lax_spacing]
+		Markdown.new(text, *options).to_html.html_safe
+	end
 end
