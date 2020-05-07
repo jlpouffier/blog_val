bff59e1a6f7cdb6111a61d85a92493c8df8b4368
jlpouffier <jean.loic.pouffier@gmail.com>
2020-05-07 20:38:26 +0200

    PICS !!!

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -69,6 +69,6 @@ class RecipesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def recipe_params
-      params.require(:recipe).permit(:title, :story, :instructions, :ingredients, :complexity, :time, :category_ids => [])
+      params.require(:recipe).permit(:title, :story, :instructions, :ingredients, :complexity, :time, :photo, :category_ids => [])
     end
 end
