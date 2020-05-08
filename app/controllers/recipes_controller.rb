bf87a2ae3b292849c06e9c397503c49f0251e161
jlpouffier <jean.loic.pouffier@gmail.com>
2020-05-09 01:21:31 +0200

    ultra basic recipe search

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,11 @@ class RecipesController < ApplicationController
   # GET /recipes
   # GET /recipes.json
   def index
-    @recipes = Recipe.all
+    if params[:search]
+      @recipes = Recipe.search(params[:search]).order("created_at DESC")
+    else
+      @recipes = Recipe.all.order("created_at DESC")
+    end
   end
 
   # GET /recipes/1
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,9 @@
 class Recipe < ApplicationRecord
 	has_and_belongs_to_many :categories
 	has_one_attached :photo
+
+	def self.search(search)
+		search_fields = ["title"]
+	end
 end
+
