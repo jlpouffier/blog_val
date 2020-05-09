374fdc57db1a1ecd308f3398c29f55f9362d0c3f
jlpouffier <jean.loic.pouffier@gmail.com>
2020-05-09 23:57:14 +0200

    VAlidate recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,14 @@
 class Recipe < ApplicationRecord
+	validates :title, presence: true
+	validates :photo, presence: true
+	validates :story, presence: true
+	validates :ingredients, presence: true
+	validates :complexity, presence: true
+	validates :instructions, presence: true
+	validates :preparation_time, presence: true
+
+	validates :complexity, numericality: { only_integer: true, :greater_than_or_equal_to => 1, :less_than_or_equal_to => 3 }
+
 	has_and_belongs_to_many :categories
 	has_one_attached :photo
 
