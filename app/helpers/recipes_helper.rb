79a56fc2e558effaf31d8f98c691cc54511dfbc9
jlpouffier <jean.loic.pouffier@gmail.com>
2020-05-10 12:56:56 +0200

    refactor helper of recipe

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,35 +1,3 @@
 module RecipesHelper
-	def format_global_time(recipe)
-		global_time = recipe.preparation_time.to_i + recipe.waiting_time.to_i + recipe.baking_time.to_i
-		hours, minutes = global_time.divmod(60)
-		if hours == 0
-			minutes.to_s + " min"
-		elsif minutes == 0
-			hours.to_s + "h"
-		else
-			hours.to_s + "h" + minutes.to_s
-		end
-	end
 
-	def format_time(integer)
-		hours, minutes = integer.to_i.divmod(60)
-		if hours == 0
-			minutes.to_s + " min"
-		elsif minutes == 0
-			hours.to_s + "h"
-		else
-			hours.to_s + "h" + minutes.to_s
-		end
-	end
-
-	def format_complexity(recipe)
-		case recipe.complexity
-		when 1
-			"Simple"
-		when 2
-			"Moyen"
-		when 3
-			"Difficile"
-		end
-	end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -16,5 +16,45 @@ class Recipe < ApplicationRecord
 	def self.search(search)
 		where("title LIKE ?", "%#{search}%") 
 	end
+
+	def preparation_time_human
+		convert_time_to_human(self.preparation_time)
+	end
+
+	def baking_time_human
+		convert_time_to_human(self.baking_time)
+	end
+
+	def waiting_time_human
+		convert_time_to_human(self.waiting_time)
+	end
+
+	def global_time_human
+		global_time = self.preparation_time.to_i + self.waiting_time.to_i + self.baking_time.to_i
+		convert_time_to_human(global_time)
+	end
+
+	def complexity_human
+		case self.complexity
+		when 1
+			"Simple"
+		when 2
+			"Moyen"
+		when 3
+			"Difficile"
+		end
+	end
+
 end
 
+
+def convert_time_to_human(integer)
+	hours, minutes = integer.to_i.divmod(60)
+	if hours == 0
+		minutes.to_s + " min"
+	elsif minutes == 0
+		hours.to_s + "h"
+	else
+		hours.to_s + "h" + minutes.to_s
+	end
+end
