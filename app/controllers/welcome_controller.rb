0dc90b417ecc541afad9eb9a5d26b565e57930c3
jlpouffier <jean.loic.pouffier@gmail.com>
2020-05-14 20:42:29 +0200

    Layout change on the home page. Last recipe bigger

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-  	@latest_published_recipes = Recipe.published.order("created_at DESC").limit(6)
+  	@latest_published_recipes = Recipe.published.order("created_at DESC").limit(3)
   	@unpublished_recipes = Recipe.unpublished.order("created_at DESC")
   end
 end
