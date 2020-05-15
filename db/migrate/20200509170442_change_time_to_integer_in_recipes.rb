21c34f5eec43d12f6b730c8d941110b4c172c8af
jlpouffier <jean.loic.pouffier@gmail.com>
2020-05-15 23:48:17 +0200

    postgress migration ..

--- db/migrate/20200509170442_change_time_to_integer_in_recipes.rb
+++ db/migrate/20200509170442_change_time_to_integer_in_recipes.rb
@@ -1,5 +1,5 @@
 class ChangeTimeToIntegerInRecipes < ActiveRecord::Migration[6.0]
   def change
-  	  change_column :recipes, :preparation_time, :integer
+  	  change_column :recipes, :preparation_time, 'integer USING cast(extract(epoch from current_timestamp) as integer)'
   end
 end
