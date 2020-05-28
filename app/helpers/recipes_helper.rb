daf0e10989f8c9fe7042cfca9c27a10577af723e
jlpouffier <jean.loic.pouffier@gmail.com>
2020-05-28 09:18:28 +0200

    migrate helpers to helpers...

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,46 @@
 module RecipesHelper
+
+	# Convert preparation time into human readable string
+	def preparation_time_human(recipe)
+		convert_time_to_human(recipe.preparation_time)
+	end
+
+	# Convert baking time into human readable string
+	def baking_time_human(recipe)
+		convert_time_to_human(recipe.baking_time)
+	end
+
+	# Convert waiting time into human readable string
+	def waiting_time_human(recipe)
+		convert_time_to_human(recipe.waiting_time)
+	end
+
+	# Convert global time into human readable string
+	def global_time_human(recipe)
+		global_time = recipe.preparation_time.to_i + recipe.waiting_time.to_i + recipe.baking_time.to_i
+		convert_time_to_human(global_time)
+	end
+
+	# Convert complixity time into human readable string
+	def complexity_human(recipe)
+		case recipe.complexity
+		when 1
+			"Simple"
+		when 2
+			"Moyen"
+		when 3
+			"Difficile"
+		end
+	end
+end
+
+def convert_time_to_human(integer)
+	hours, minutes = integer.to_i.divmod(60)
+	if hours == 0
+		minutes.to_s + " min"
+	elsif minutes == 0
+		hours.to_s + "h"
+	else
+		hours.to_s + "h" + minutes.to_s
+	end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -41,49 +41,4 @@ class Recipe < ApplicationRecord
 	def self.random
 		offset(rand(count))
     end
-
-    # Convert preparation time into human readable string
-	def preparation_time_human
-		convert_time_to_human(self.preparation_time)
-	end
-
-	# Convert baking time into human readable string
-	def baking_time_human
-		convert_time_to_human(self.baking_time)
-	end
-
-	# Convert waiting time into human readable string
-	def waiting_time_human
-		convert_time_to_human(self.waiting_time)
-	end
-
-	# Convert global time into human readable string
-	def global_time_human
-		global_time = self.preparation_time.to_i + self.waiting_time.to_i + self.baking_time.to_i
-		convert_time_to_human(global_time)
-	end
-
-	# Convert complixity time into human readable string
-	def complexity_human
-		case self.complexity
-		when 1
-			"Simple"
-		when 2
-			"Moyen"
-		when 3
-			"Difficile"
-		end
-	end
-end
-
-# Convert an number of minutes into a human readable time
-def convert_time_to_human(integer)
-	hours, minutes = integer.to_i.divmod(60)
-	if hours == 0
-		minutes.to_s + " min"
-	elsif minutes == 0
-		hours.to_s + "h"
-	else
-		hours.to_s + "h" + minutes.to_s
-	end
 end
